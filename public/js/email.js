1f57e818ec25ad18e1a41b043b015e2fc7e15b17
Helmut Cardenas <aleksey89col@gmail.com>
2018-01-26 23:14:53 -0600

    fixed toast for better user interface

--- public/js/email.js
+++ public/js/email.js
@@ -1,12 +1,6 @@
 $(document).ready(function() {
 	console.log("ready");
-	$.toast({
-				    heading: 'Email',
-				    text: 'Email Sent.',
-				    icon: 'info',
-				    loader: true,  // To change the background
-				    position: "top-left"
-				})
+	
 
 	$("#EmailBtn").on("click", ()=> {
 		event.preventDefault();
@@ -18,32 +12,76 @@ $(document).ready(function() {
 			text: $("#emailText").val().trim(),
 			subject: $("#emailSubject").val().trim()
 		};
-		console.log(emailInfo);
-		$.post( "/api/email", emailInfo ,function( response ) {
-  			console.log(response);
-  			if (response === "sent") {
-	  			$.toast({
-				    heading: 'Success',
-				    text: 'Email Sent.',
-				    icon: 'info',
-				    loader: true,
-				    position: "top-left"  
-				})
-				
-			} else {
-				$.toast({
+		
+		var arr = checkEmailForm(emailInfo);
+
+		if (arr.length !== 0) {
+			$.toast({
 				    heading: 'Error',
-				    text: 'Email Failed.',
+				    text: arr,
 				    icon: 'error',
 				    loader: true,
-				    position: "top-left" 
+				    position: "top-left"
 				})
-				
-			}
+		} else {
 
-			$("#emailForm")[0].reset();
-		});
+			$.post( "/api/email", emailInfo ,function( response ) {
+	  			console.log(response);
+	  			if (response === "sent") {
+		  			$.toast({
+					    heading: 'Success',
+					    text: 'Email Sent.',
+					    icon: 'info',
+					    loader: true,
+					    position: "top-left"  
+					});
 
-		
+					$("#emailName").removeClass("uk-form-danger");
+					$("#emailFrom").removeClass("uk-form-danger");
+					$("#emailText").removeClass("uk-form-danger");
+					$("#emailSubject").removeClass("uk-form-danger");
+					
+				} else {
+					$.toast({
+					    heading: 'Error',
+					    text: 'Email Failed.',
+					    icon: 'error',
+					    loader: true,
+					    position: "top-left" 
+					})
+					
+				}
+
+				$("#emailForm")[0].reset();
+			});
+
+		}
 	});
+	
 });
+
+
+function checkEmailForm (emailInfo) {
+	var errors = [];
+
+	if (emailInfo.name === "") {
+		errors.push("Name cannot be empty");
+		$("#emailName").addClass("uk-form-danger");	
+	}
+	if (emailInfo.from === "") {
+		errors.push("Email cannot be empty");
+		$("#emailFrom").addClass("uk-form-danger");
+	}
+	if (emailInfo.text === "") {
+		errors.push("Message cannot be empty");
+		$("#emailText").addClass("uk-form-danger");
+	}
+	if (emailInfo.subject === "") {
+		errors.push("Subject cannot be empty");
+		$("#emailSubject").addClass("uk-form-danger");
+	}
+
+
+
+	return errors;
+}
